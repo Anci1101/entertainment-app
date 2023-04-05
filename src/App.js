e91266d11590b44a34ace11a4e899d14823c37bb
Ana <anci11011@gmail.com>
2023-04-05 11:47:52 +0200

    add navbar for all screen sizes, add react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar.js';
+import Home from './pages/Home.js'
+import Movies from './pages/Movies.js'
+import TvSeries from './pages/TvSeries.js'
+import Bookmarked from './pages/Bookmarked.js'
 import './scss/scss-style.js'
 function App() {
 
   return (
-    <div className="App">
+    <div className="app">
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/movies' element={<Movies/>}/>
+          <Route path='/tvSeries' element={<TvSeries/>}/>
+          <Route path='/bookmarked' element={<Bookmarked/>}/>
+          <Route path='*' element={<h1>PAGE NOT FOUND</h1>}/>
+        </Routes>
+      </Router>
       Hello world!
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {ReactComponent as HomeIcon} from '../assets/icon-nav-home.svg'
+import {ReactComponent as MoviesIcon} from '../assets/icon-nav-movies.svg'
+import {ReactComponent as TvSeriesIcon} from '../assets/icon-nav-tv-series.svg'
+import {ReactComponent as BookmarkIcons} from '../assets/icon-nav-bookmark.svg'
+import {ReactComponent as Logo} from '../assets/logo.svg'
+import AvatarIcon from '../assets/image-avatar.png'
+import '../scss/scss-style'
+
+const NavBar = () => {
+  return (
+    <div className='navBarContainer'>
+        <div className='navBar'>
+            <Logo className='logInSingUpIcon'/>
+            <div className='menuWrapper'>
+                <Link to='/'>
+                    <HomeIcon className='homeIcon'/>
+                </Link>
+                <Link to='/movies'>
+                    <MoviesIcon className='moviesIcon'/>
+                </Link>
+                <Link to='/tvSeries'>
+                    <TvSeriesIcon className='tvSeriesIcon'/>
+                </Link>
+                <Link to='/bookmarked'>
+                    <BookmarkIcons className='bookmarkIcon'/>
+                </Link>
+            </div>
+            <div className='avatarIcon'>
+                <img src={AvatarIcon} alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
